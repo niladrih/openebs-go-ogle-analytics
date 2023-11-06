3cebee9bb206943fa52f4b5b5b11a934653bf99a
Abhinandan Purkait <purkaitabhinandan@gmail.com>
2023-11-06 12:48:12 +0530

    fix: replaced hypen with underscore in event name
    Signed-off-by: Niladri Halder <niladri.halder26@gmail.com>

--- payload/build.go
+++ payload/build.go
@@ -46,7 +46,7 @@ func WithClientId(clientId string) PayloadOption {
 func WithOpenebsEvent(event *event.OpenebsEvent) PayloadOption {
 	return func(p *Payload) error {
 		p.Events = append(p.Events, ApiEvent{
-			Name:   event.CategoryStr() + "-" + event.ActionStr(),
+			Name:   event.CategoryStr() + "_" + event.ActionStr(),
 			Params: *event,
 		})
 		return nil
